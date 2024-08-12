e2779f19df488972ab2cf27ab15ceb967e5a35a2
vishnuforcode <vishnuforgrow@gmail.com>
2024-08-12 19:10:55 +0530

    added comment to chai.jsx

--- ViteReact/src/chai.jsx
+++ ViteReact/src/chai.jsx
@@ -5,3 +5,7 @@ function Chai(){
 }
 
 export default Chai
+
+
+
+ // <></> ye fragmants h , jsx kehta ha mere andar sirf ek hi return hoga , to hum ek hi krre h , uske andar aur daal skte ha (closing tag se endhona)
