a69323638843810fddf3c4aef23518ed26b38864
vishnuforcode <vishnuforgrow@gmail.com>
2024-08-12 19:08:47 +0530

    sample commit

--- ViteReact/src/App.jsx
+++ ViteReact/src/App.jsx
@@ -5,8 +5,12 @@ function App() {
  
 
   return (
+    <>  
    <Chai/>
+   </>
   )
+
+  // <></> ye fragmants h , jsx kehta ha mere andar sirf ek hi return hoga , to hum ek hi krre h , uske andar aur daal skte ha (closing tag se endhona chahiye)
 }
 
 export default App
